0a87f64ec6e7e7144649e2e0d7ea7a483de73fe2
Muhammad Asghar Ali <51094535+muhammad-asghar-ali@users.noreply.github.com>
2023-03-11 19:57:52 +0500

    feat(): working with nestjs pipes and filter and implement (#1)
    Co-authored-by: apple <apple@apples-MacBook-Pro.local>

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,6 @@
 import {
+  BadRequestException,
+  Body,
   Controller,
   Delete,
   Get,
@@ -7,12 +9,15 @@ import {
   Post,
   Req,
   Res,
+  UseFilters,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { CreateUserDto, UserParamsDto } from './dtos/user.dto';
+import { HttpExceptionFilter } from './filter/http-exception.filter';
 import { User } from './interface/user.interface';
+import { JoiValidationPipe } from './pipes/validation.pipe';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -26,7 +31,18 @@ export class UsersController {
 
   @Post()
   @UsePipes(new ValidationPipe())
+  // @UsePipes(new JoiValidationPipe())
   createUser(user: CreateUserDto): User {
+    return this._svc.createUser(user);
+  }
+
+  // with class validator
+  @Post()
+  @UsePipes(new ValidationPipe())
+  // @UsePipes(new JoiValidationPipe())
+  createUserWithValidator(
+    @Body(new ValidationPipe()) user: CreateUserDto,
+  ): User {
     return this._svc.createUser(user);
   }
 
@@ -36,12 +52,17 @@ export class UsersController {
   }
 
   @Get('find/:email')
-  getUser(@Param() params: UserParamsDto): User {
-    return this._svc.getUser(params.email);
+  @UseFilters(new HttpExceptionFilter())
+  async getUser(@Param() params: UserParamsDto): Promise<User> {
+    try {
+      return this._svc.getUser(params.email);
+    } catch (err) {
+      throw new BadRequestException('test');
+    }
   }
 
   @Get('find/:email')
-  getUserByEmail(@Param('email') email: string): User {
+  async getUserByEmail(@Param('email') email: string): Promise<User> {
     return this._svc.getUser(email);
   }
 
