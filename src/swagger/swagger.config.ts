2675852456b0856f41fe68181338abb4a6c217fc
Muhammad Asghar Ali <51094535+muhammad-asghar-ali@users.noreply.github.com>
2023-03-20 17:57:54 +0500

    feat(): implement swagger in application (#4)
    * feat(): working with nestjs pipes and filter and implement
    
    * feat(): working with guards and interceptors in nestjs
    
    * feat(): working on task module and separate the core chapter of nest js
    
    * feat(): implement swagger in application
    
    * feat(): implement swagger in application
    
    ---------
    
    Co-authored-by: apple <apple@apples-MacBook-Pro.local>

---
+++ src/swagger/swagger.config.ts
@@ -0,0 +1,8 @@
+import { swaggerConfig } from './swagger.interface';
+
+export const SWAGGER_CONFIG: swaggerConfig = {
+  title: 'Nest js Template',
+  description: 'Template',
+  version: '1.0',
+  tags: ['task'],
+};
---
+++ src/swagger/swagger.interface.ts
@@ -0,0 +1,6 @@
+export interface swaggerConfig {
+  title: string;
+  description: string;
+  version: string;
+  tags: string[];
+}
