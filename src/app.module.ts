ff40391a5e09686a2aedc331ee99cd8a920354e8
Muhammad Asghar Ali <51094535+muhammad-asghar-ali@users.noreply.github.com>
2023-03-22 11:55:59 +0500

    feat(): working a test module and create common file for app and working with swagger docs and look detail into dtos validations (#6)
    * feat(): working with nestjs pipes and filter and implement
    
    * feat(): working with guards and interceptors in nestjs
    
    * feat(): working on task module and separate the core chapter of nest js
    
    * feat(): implement swagger in application
    
    * feat(): implement swagger in application
    
    * feat(): working with nosql database mongodb and create REST API for customer
    
    * feat(): working a test module and create common file for app and working with swagger docs and look detail into dtos validations
    
    ---------
    
    Co-authored-by: apple <apple@apples-MacBook-Pro.local>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cat/domain/cat.module';
 import { CustomerModule } from './customer/customer.module';
 import { DatabaseModule } from './database/database.module';
 import { TaskModule } from './task/task.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, TaskModule, DatabaseModule, CustomerModule],
+  imports: [
+    UsersModule,
+    TaskModule,
+    DatabaseModule,
+    CustomerModule,
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
