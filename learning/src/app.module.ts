f01d9c007b86c916849a449cd938f95be65d9122
apple <apple@apples-MacBook-Pro.local>
2023-03-23 12:55:04 +0500

    feat(): re-structure the code

--- learning/src/app.module.ts
+++ learning/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cat/domain/cat.module';
@@ -7,8 +9,23 @@ import { DatabaseModule } from './database/database.module';
 import { TaskModule } from './task/task.module';
 import { UsersModule } from './users/users.module';
 
+const TYPEORM_CONFIG: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'postgres',
+  autoLoadEntities: true,
+  namingStrategy: new SnakeNamingStrategy(),
+  entities: [],
+  synchronize: true,
+  logging: ['query', 'error', 'warn'],
+};
+
 @Module({
   imports: [
+    TypeOrmModule.forRoot(TYPEORM_CONFIG),
     UsersModule,
     TaskModule,
     DatabaseModule,
