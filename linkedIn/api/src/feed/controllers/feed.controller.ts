844fd1c868e9fc900fd42b81c96eb1b25fa9096e
Muhammad Asghar Ali <51094535+muhammad-asghar-ali@users.noreply.github.com>
2023-04-30 13:31:53 +0500

    feat(): complete the CRUD operations for feed module (#11)
    * feat(): working with nestjs pipes and filter and implement
    
    * feat(): working with guards and interceptors in nestjs
    
    * feat(): working on task module and separate the core chapter of nest js
    
    * feat(): implement swagger in application
    
    * feat(): implement swagger in application
    
    * feat(): working with nosql database mongodb and create REST API for customer
    
    * feat(): working a test module and create common file for app and working with swagger docs and look detail into dtos validations
    
    * feat(): re-structure the code
    
    * feat(): re-structure the code
    
    * feat(): working on linkedIn clone and initail commit
    
    * feat(): connect postgres database and working on feed module
    
    * feat(): complete the CRUD operations for feed module
    
    ---------
    
    Co-authored-by: apple <apple@apples-MacBook-Pro.local>

--- linkedIn/api/src/feed/controllers/feed.controller.ts
+++ linkedIn/api/src/feed/controllers/feed.controller.ts
@@ -1,7 +1,84 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { FeedPost } from '../models/post.interface';
 import { FeedService } from '../services/feed.service';
 
 @Controller('feed')
 export class FeedController {
   constructor(private readonly _svc: FeedService) {}
+
+  @Post()
+  public async create(@Body() feed: FeedPost, @Res() res): Promise<FeedPost> {
+    const result = await this._svc.create(feed);
+
+    return res.status(HttpStatus.CREATED).json({
+      status: 'OK',
+      message: 'Record created Successfully',
+      data: result,
+    });
+  }
+
+  @Get()
+  public async all(@Res() res): Promise<FeedPost> {
+    const result = await this._svc.all();
+
+    return res.status(HttpStatus.OK).json({
+      status: 'OK',
+      message: 'Record get Successfully',
+      data: result,
+    });
+  }
+
+  @Get(':id')
+  public async findById(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Res() res,
+  ): Promise<FeedPost> {
+    const result = await this._svc.findById(id);
+
+    return res.status(HttpStatus.OK).json({
+      status: 'OK',
+      message: 'Record get Successfully',
+      data: result,
+    });
+  }
+
+  @Delete(':id')
+  public async delete(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Res() res,
+  ): Promise<FeedPost> {
+    const result = await this._svc.delete(id);
+
+    return res.status(HttpStatus.OK).json({
+      status: 'OK',
+      message: 'Record deleted Successfully',
+      data: result,
+    });
+  }
+
+  @Patch(':id')
+  public async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() req: { body: string },
+    @Res() res,
+  ): Promise<FeedPost> {
+    const result = await this._svc.update(id, req.body);
+
+    return res.status(HttpStatus.OK).json({
+      status: 'OK',
+      message: 'Record deleted Successfully',
+      data: result,
+    });
+  }
 }
