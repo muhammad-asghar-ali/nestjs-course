3f5e5cbddb304e1619ac93303293341d7fe0a957
asgr <masgharali.eng@gmail.com>
2023-05-07 00:46:13 +0500

    feat(): working on custom guard for feed posts

--- linkedIn/api/src/feed/feed.module.ts
+++ linkedIn/api/src/feed/feed.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { FeedController } from './controllers/feed.controller';
+import { IsCreatorGuard } from './guard/is-creator.guard';
 import { FeedEntity } from './models/post.entity';
 import { FeedService } from './services/feed.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FeedEntity])],
+  imports: [TypeOrmModule.forFeature([FeedEntity]), AuthModule],
   controllers: [FeedController],
-  providers: [FeedService],
+  providers: [FeedService, IsCreatorGuard],
 })
 export class FeedModule {}
